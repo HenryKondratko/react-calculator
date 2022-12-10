bfa5b19ee467abda7e46b88c171b36e0976852c1
Henry Kondratko <henry.kondratko@gmail.com>
2022-12-10 11:23:22 -0500

    fixed error with displaying value

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, {useState} from "react";
-import "./App.css";
+import React, { useState } from "react";
+
 import Wrapper from "./components/Wrapper";
 import Screen from "./components/Screen";
 import ButtonBox from "./components/ButtonBox";
@@ -7,38 +7,43 @@ import Button from "./components/Button";
 
 const btnValues = [
     ["C", "+-", "%", "/"],
-    ["7", "8", "9", "X"],
-    ["4", "5", "6", "-"],
-    ["1", "2", "3", "+"],
-    ["0", ".", "="],
+    [7, 8, 9, "X"],
+    [4, 5, 6, "-"],
+    [1, 2, 3, "+"],
+    [0, ".", "="],
 ];
 
-function App() {
+const toLocaleString = (num) =>
+    String(num).replace(/(?<!\..*)(\d)(?=(?:\d{3})+(?:\.|$))/g, "$1 ");
+
+const removeSpaces = (num) => num.toString().replace(/\s/g, "");
+
+const math = (a, b, sign) =>
+    sign === "+" ? a + b : sign === "-" ? a - b : sign === "X" ? a * b : a / b;
+
+const App = () => {
     let [calc, setCalc] = useState({
         sign: "",
         num: 0,
         res: 0,
-    })
+    });
 
-    function numClickHandler(e) {
+    const numClickHandler = (e) => {
         e.preventDefault();
         const value = e.target.innerHTML;
-
-        if (calc.num.length < 16) {
+        if (removeSpaces(calc.num).length < 16) {
             setCalc({
                 ...calc,
                 num:
-                    calc.num === 0 && value === "0"
-                        ? "0"
-                        : calc.num % 1 === 0
-                            ? Number(calc.num + value)
-                            : calc.num + value,
+                    removeSpaces(calc.num) % 1 === 0 && !calc.num.toString().includes(".")
+                        ? toLocaleString(Number(removeSpaces(calc.num + value)))
+                        : toLocaleString(calc.num + value),
                 res: !calc.sign ? 0 : calc.res,
-            })
+            });
         }
-    }
+    };
 
-    function decimalClickHandler(e) {
+    const comaClickHandler = (e) => {
         e.preventDefault();
         const value = e.target.innerHTML;
 
@@ -46,106 +51,107 @@ function App() {
             ...calc,
             num: !calc.num.toString().includes(".") ? calc.num + value : calc.num,
         });
-    }
+    };
 
-    function signClickHandler(e) {
-        e.preventDefault();
-        const value = e.target.innerHTML;
-
+    const signClickHandler = (e) => {
         setCalc({
             ...calc,
-            sign: value,
-            res: !calc.res && calc.num ? calc.num : calc.res,
+            sign: e.target.innerHTML,
+            res: !calc.num
+                ? calc.res
+                : !calc.res
+                    ? calc.num
+                    : toLocaleString(
+                        math(
+                            Number(removeSpaces(calc.res)),
+                            Number(removeSpaces(calc.num)),
+                            calc.sign
+                        )
+                    ),
             num: 0,
-        })
-    }
+        });
+    };
 
-    function equalsClickHandler() {
+    const equalsClickHandler = () => {
         if (calc.sign && calc.num) {
-            const math = (a, b, sign) =>
-                sign === "+"
-                    ? a + b
-                    : sign === "-"
-                        ? a - b
-                        : sign === "X"
-                            ? a * b
-                            : a / b;
-
             setCalc({
                 ...calc,
                 res:
                     calc.num === "0" && calc.sign === "/"
                         ? "Can't divide with 0"
-                        : math(Number(calc.res), Number(calc.num), calc.sign),
+                        : toLocaleString(
+                            math(
+                                Number(removeSpaces(calc.res)),
+                                Number(removeSpaces(calc.num)),
+                                calc.sign
+                            )
+                        ),
                 sign: "",
                 num: 0,
             });
         }
-    }
+    };
 
-    function percentClickHandler() {
-        let num = calc.num ? parseFloat(calc.num) : 0;
-        let res = calc.res ? parseFloat(calc.res) : 0;
+    const invertClickHandler = () => {
+        setCalc({
+            ...calc,
+            num: calc.num ? toLocaleString(removeSpaces(calc.num) * -1) : 0,
+            res: calc.res ? toLocaleString(removeSpaces(calc.res) * -1) : 0,
+            sign: "",
+        });
+    };
 
+    const percentClickHandler = () => {
+        let num = calc.num ? parseFloat(removeSpaces(calc.num)) : 0;
+        let res = calc.res ? parseFloat(removeSpaces(calc.res)) : 0;
         setCalc({
             ...calc,
             num: (num /= Math.pow(100, 1)),
             res: (res /= Math.pow(100, 1)),
             sign: "",
         });
-    }
+    };
 
-    function invertClickHandler() {
-        setCalc({
-            ...calc,
-            num: calc.num ? calc.num * -1 : 0,
-            res: calc.res ? calc.res * -1 : 0,
-            sign: "",
-        });
-    }
-
-    function resetClickHandler() {
+    const resetClickHandler = () => {
         setCalc({
             ...calc,
             sign: "",
             num: 0,
             res: 0,
         });
-    }
+    };
 
     return (
         <Wrapper>
-            <Screen value={calc.num ? calc.num : calc.res}/>
+            <Screen value={calc.num ? calc.num : calc.res} />
             <ButtonBox>
-                {
-                    btnValues.flat().map((btn, i) => {
-                        return (
-                            <Button
-                                key={i}
-                                className={btn === "=" ? "equals" : ""}
-                                value={btn}
-                                onClick={
-                                    btn === "C"
-                                        ? resetClickHandler
-                                        : btn === "+-"
-                                            ? invertClickHandler
-                                            : btn === "%"
-                                                ? percentClickHandler
-                                                : btn === "="
-                                                    ? equalsClickHandler
-                                                    : btn === "/" || btn === "X" || btn === "-" || btn === "+"
-                                                        ? signClickHandler
-                                                        : btn === "."
-                                                            ? decimalClickHandler
-                                                            : numClickHandler
-                                }
-                            />
-                        );
-                    })
-                }
+                {btnValues.flat().map((btn, i) => {
+                    return (
+                        <Button
+                            key={i}
+                            className={btn === "=" ? "equals" : ""}
+                            value={btn}
+                            onClick={
+                                btn === "C"
+                                    ? resetClickHandler
+                                    : btn === "+-"
+                                        ? invertClickHandler
+                                        : btn === "%"
+                                            ? percentClickHandler
+                                            : btn === "="
+                                                ? equalsClickHandler
+                                                : btn === "/" || btn === "X" || btn === "-" || btn === "+"
+                                                    ? signClickHandler
+                                                    : btn === "."
+                                                        ? comaClickHandler
+                                                        : numClickHandler
+                            }
+                        />
+                    );
+                })}
             </ButtonBox>
         </Wrapper>
     );
-}
+};
 
 export default App;
